15c5703a9b1ad343a62d9db6dbffe3c3ee59bf25
Andrew Chen <achen26@ncsu.edu>
2024-11-22 14:12:22 -0500

    create main function to start client node and check for errors if wrong number of args is sent

--- src/cpp_srvcli/src/add_two_ints_client.cpp
+++ src/cpp_srvcli/src/add_two_ints_client.cpp
@@ -8,3 +8,12 @@
 // This namespace allows us to write times in our code easily ie. we can write 5s to mean 5 seconds.
 using namespace std::chrono_literals;
 
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc,argv);
+
+  if (argc != 3)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"usage: add_two_ints_client X Y ");
+  }
+}
