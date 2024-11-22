1fa70b952b26b077c228c09228def4315cb8c9dc
Andrew Chen <achen26@ncsu.edu>
2024-11-22 14:08:43 -0500

    added chrono_literals namespace to write delays more easily

--- src/cpp_srvcli/src/add_two_ints_client.cpp
+++ src/cpp_srvcli/src/add_two_ints_client.cpp
@@ -5,3 +5,6 @@
 #include <cstdlib>
 #include <memory>
 
+// This namespace allows us to write times in our code easily ie. we can write 5s to mean 5 seconds.
+using namespace std::chrono_literals;
+
