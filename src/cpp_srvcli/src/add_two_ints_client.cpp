e067c170ba934e795392e80b6ef582d3c42e5028
Andrew Chen <achen26@ncsu.edu>
2024-11-22 16:07:59 -0500

    corrected typo in service name documented arg parsing for client node

--- src/cpp_srvcli/src/add_two_ints_client.cpp
+++ src/cpp_srvcli/src/add_two_ints_client.cpp
@@ -19,6 +19,7 @@ int main(int argc, char **argv)
   if (argc != 3)
   {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"usage: add_two_ints_client X Y ");
+    return 1;
   }
 
   // We are create a shared ptr to a node we just constructed called add_two_ints_client
@@ -32,11 +33,15 @@ int main(int argc, char **argv)
 
   // We are creating a client shared_ptr by calling the create_client method of the Node class
   rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client = 
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints_client");
+    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
 
   auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-  request->a = atoll(argv[1]);
-  request->b = atoll(argv[2]);
+
+  // This parses the arguments we pass when 
+  // starting the executable ie. ros2 run cpp_srvcli client argv[1] argv[2]
+  
+  request->a = atoll(argv[1]); //arg 1 when the client sends a request
+  request->b = atoll(argv[2]); //arg 2 when the client sends a request
 
   // This while gives the client one second to see if there are any services in the network. If
   // it doesn't find any then it will keep waiting. If the client is cancelled by you pressing 
